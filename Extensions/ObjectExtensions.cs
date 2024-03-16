b67e7dd3dd7772c72d9620b80a1ee23379355802
AlexSJC <cn_sjc@outlook.com>
2024-03-16 11:28:46 +0800

    Update.

---
+++ Extensions/ObjectExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Asjc.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static object ChangeType(this object value, Type type)
+        {
+            try
+            {
+                return Convert.ChangeType(value, type);
+            }
+            catch
+            {
+                return default;
+            }
+        }
+    }
+}

bc548b76cd2de7a050e36ef43b373fcfb4367e3d
AlexSJC <cn_sjc@outlook.com>
2024-04-18 21:38:08 +0800

    添加 IEnumerableExtensions

---
+++ Extensions.Tests/ObjectExtensionsTests.cs
@@ -0,0 +1,14 @@
+﻿namespace Asjc.Extensions.Tests
+{
+    [TestClass]
+    public class ObjectExtensionsTests
+    {
+        [TestMethod]
+        public void ChangeTypeTest()
+        {
+            string s = "1";
+            int i = (int)s.ChangeType(typeof(int));
+            Assert.AreEqual(1, i);
+        }
+    }
+}
---
+++ Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Asjc.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static bool ContentEqual<T>(this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            if (first.Count() != second.Count())
+                return false;
+            return first.OrderBy(x => x).SequenceEqual(second.OrderBy(x => x));
+        }
+    }
+}

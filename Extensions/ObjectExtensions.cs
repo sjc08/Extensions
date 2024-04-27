8613997c47608b19d8874b1367e66b45b210dfe5
AlexSJC <cn_sjc@outlook.com>
2024-04-27 22:11:20 +0800

    update

--- Extensions/ObjectExtensions.cs
+++ Extensions/ObjectExtensions.cs
@@ -4,7 +4,12 @@ namespace Asjc.Extensions
 {
     public static class ObjectExtensions
     {
+#if NET8_0
+#nullable enable
+        public static object? ChangeType(this object? value, Type type)
+#else
         public static object ChangeType(this object value, Type type)
+#endif
         {
             try
             {
--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -1,13 +1,39 @@
 ﻿using System;
+using System.Text;
 
 namespace Asjc.Extensions
 {
     public static class StringExtensions
     {
-#if NETCOREAPP2_0_OR_GREATER
+#if NET8_0
+#nullable enable
+        public static string Replace(this string str, string oldValue, string? newValue, bool ignoreCase)
+#else
         public static string Replace(this string str, string oldValue, string newValue, bool ignoreCase)
+#endif
         {
             return str.Replace(oldValue, newValue, ignoreCase ? StringComparison.CurrentCultureIgnoreCase : StringComparison.CurrentCulture);
+        }
+
+#if NETSTANDARD2_0
+        public static string Replace(this string str, string oldValue, string newValue, StringComparison comparison)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int previousIndex = 0;
+            int index = str.IndexOf(oldValue, comparison);
+            while (index != -1)
+            {
+                sb.Append(str.Substring(previousIndex, index - previousIndex));
+                sb.Append(newValue);
+                index += oldValue.Length;
+
+                previousIndex = index;
+                index = str.IndexOf(oldValue, index, comparison);
+            }
+            sb.Append(str.Substring(previousIndex));
+
+            return sb.ToString();
         }
 #endif
     }

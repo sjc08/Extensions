fc49e90fe08a786c09ef242a7a8ef0e4d969a3d7
AlexSJC <cn_sjc@outlook.com>
2024-07-14 15:08:12 +0800

    update

--- Extensions/ObjectExtensions.cs
+++ Extensions/ObjectExtensions.cs
@@ -21,17 +21,45 @@ namespace Asjc.Extensions
             => Try(() => Convert.ChangeType(value, typeCode), out result);
 
 #if NETCOREAPP3_0_OR_GREATER
-        public static bool ConvertTo(this object? value, Type conversionType, IFormatProvider provider, out object? result)
+        public static bool ConvertTo(this object? value, Type conversionType, IFormatProvider? provider, out object? result)
 #else
         public static bool ConvertTo(this object value, Type conversionType, IFormatProvider provider, out object result)
 #endif
             => Try(() => Convert.ChangeType(value, conversionType, provider), out result);
 
 #if NETCOREAPP3_0_OR_GREATER
-        public static bool ConvertTo(this object? value, TypeCode typeCode, IFormatProvider provider, out object? result)
+        public static bool ConvertTo(this object? value, TypeCode typeCode, IFormatProvider? provider, out object? result)
 #else
         public static bool ConvertTo(this object value, TypeCode typeCode, IFormatProvider provider, out object result)
 #endif
             => Try(() => Convert.ChangeType(value, typeCode, provider), out result);
+
+#if NETCOREAPP3_0_OR_GREATER
+        public static object? ConvertTo(this object? value, Type conversionType)
+#else
+        public static object ConvertTo(this object value, Type conversionType)
+#endif
+            => Try(() => Convert.ChangeType(value, conversionType));
+
+#if NETCOREAPP3_0_OR_GREATER
+        public static object? ConvertTo(this object? value, TypeCode typeCode)
+#else
+        public static object ConvertTo(this object value, TypeCode typeCode)
+#endif
+            => Try(() => Convert.ChangeType(value, typeCode));
+
+#if NETCOREAPP3_0_OR_GREATER
+        public static object? ConvertTo(this object? value, Type conversionType, IFormatProvider? provider)
+#else
+        public static object ConvertTo(this object value, Type conversionType, IFormatProvider provider)
+#endif
+            => Try(() => Convert.ChangeType(value, conversionType, provider));
+
+#if NETCOREAPP3_0_OR_GREATER
+        public static object? ConvertTo(this object? value, TypeCode typeCode, IFormatProvider? provider)
+#else
+        public static object ConvertTo(this object value, TypeCode typeCode, IFormatProvider provider)
+#endif
+            => Try(() => Convert.ChangeType(value, typeCode, provider));
     }
 }
